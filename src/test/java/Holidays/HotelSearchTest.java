84c90eb03db756ebc277009b8e9e20f936e9f0c8
Magdalena Zagwojska <magdalena.zagwojska@gmail.com>
2021-09-22 13:59:28 +0200

    add assertions to test

--- src/test/java/Holidays/HotelSearchTest.java
+++ src/test/java/Holidays/HotelSearchTest.java
@@ -2,8 +2,13 @@ package Holidays;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class HotelSearchTest {
 
@@ -23,12 +28,29 @@ public class HotelSearchTest {
         driver.findElement(By.cssSelector("span[class='select2-match']")).click();
 
         driver.findElement(By.name("checkin")).sendKeys("16/09/2021");
-        driver.findElement(By.name("checkout")).sendKeys("16/10/2021");
+        driver.findElement(By.name("checkout")).click();
+        driver.findElements(By.xpath("//td[@class='day ' and text()='21']"))
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
 
         driver.findElement(By.id("travellersInput")).click();
         driver.findElement(By.id("adultPlusBtn")).click();
         driver.findElement(By.id("childInput")).clear();
         driver.findElement(By.id("childInput")).sendKeys("3");
+
+        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+
+        List<String> hotelNames = driver.findElements(By.cssSelector("[class*='list_title'] b"))
+                .stream()
+                .map(el -> el.getAttribute("textContent"))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals("Jumeirah Beach Hotel", hotelNames.get(0));
+        Assert.assertEquals("Oasis Beach Tower", hotelNames.get(1));
+        Assert.assertEquals("Rose Rayhaan Rotana", hotelNames.get(2));
+        Assert.assertEquals("Hyatt Regency Perth", hotelNames.get(3));
 
 
     }
